b25bbd68f9559ff01b1c6409f2f3aa4a0764fc46
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2019-12-12 11:07:58 +0700

    Penambahan

--- exercises-16.js
+++ exercises-16.js
@@ -9,7 +9,7 @@ function  totalDigitRekursif(angka) {
   
   // TEST CASES
   console.log(totalDigitRekursif(512)); // 8
-//   console.log(totalDigitRekursif(1542)); // 12
-//   console.log(totalDigitRekursif(5)); // 5
-//   console.log(totalDigitRekursif(21)); // 3
-//   console.log(totalDigitRekursif(11111)); // 5
+  console.log(totalDigitRekursif(1542)); // 12
+  console.log(totalDigitRekursif(5)); // 5
+  console.log(totalDigitRekursif(21)); // 3
+  console.log(totalDigitRekursif(11111)); // 5
